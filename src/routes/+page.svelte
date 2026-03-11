6330aee443d7372a002c802e4d56d729c7e2a08f
yawn west <yawn.west@posteo.eu>
2026-03-11 22:11:16 +0100

    added directory picker

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,20 @@
 <script lang="ts">
+	import { open } from '@tauri-apps/plugin-dialog';
+
+	let path: string = $state('No directory selected');
+
+	async function selectDirectory() {
+		const selected = await open({
+			directory: true
+		});
+
+		path = selected ?? '';
+	}
 </script>
 
-<main>hello</main>
+<main>
+	<button onclick={selectDirectory} type="button" class="btn preset-filled-primary-500">
+		Directory...
+	</button>
+	<span class="text text-primary-500">{path}</span>
+</main>
